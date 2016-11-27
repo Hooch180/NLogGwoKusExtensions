d1db27b63961e6a789288e4326e68a2a38235697
Maciej Kuśnierz <maciej.kusnierz@euvic.pl>
2016-11-27 15:56:28 +0100

    Initial commit.

---
+++ NLogGwoKusExtensions.Tests/SlackBotTargetTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NLogGwoKusExtensions.Tests
+{
+
+	public class SlackBotTargetTests
+	{
+		private string apiKey;
+
+		public SlackBotTargetTests()
+		{
+			apiKey = "botApiKey";
+		}
+
+		[Fact]
+		public void HelloWorld()
+		{
+			Assert.StartsWith("Hello", "Hello World!");
+		}
+	}
+}
---
+++ NLogGwoKusExtensions/SlackBotTarget.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+
+namespace NLogGwoKusExtensions
+{
+	[Target("SlackBotTarget")]
+	public sealed class SlackBotTarget : TargetWithLayout
+	{
+		[RequiredParameter]
+		public string ApiKey { get; set; }
+
+		public SlackBotTarget()
+		{
+			
+		}
+
+		protected override void Write(LogEventInfo logEvent)
+		{
+			base.Write(logEvent);
+		}
+	}
+}
